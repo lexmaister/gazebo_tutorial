1e5225e7e53d71b7d7d52d2f10d1fb57b4890c2e
Alex Aleev <aleev@simicon.com>
2026-01-11 14:38:09 +0300

    feat: add separate launch to spawn target

---
+++ 07_moveit_task_constructor/panda_mtc_py/launch/spawn_target.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+PKG = "panda_gz_moveit"
+
+
+def generate_launch_description():
+    target = PathJoinSubstitution([FindPackageShare(PKG), "sdf", "target.sdf"])
+
+    gz_spawn_target = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-file",
+            target,
+            "-name",
+            "target",
+            "-x",
+            "0.45",
+            "-y",
+            "0.3",
+            "-z",
+            "0.36",
+        ],
+    )
+
+    return LaunchDescription([gz_spawn_target])
